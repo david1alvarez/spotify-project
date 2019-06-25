44c128976ed071b3e1c8e76547011a448f915ca7
david1alvarez <dalvarez@predictiveindex.com>
2019-06-25 14:13:12 -0400

    update to get authcode from api

--- spotifyApi.connect.py
+++ spotifyApi.connect.py
@@ -1,18 +1,25 @@
 import requests
 from getpass import getpass
+# import json
+import base64
 
 clientId = '1bef273e723c4710b8befe9933f7aa36'
 privateKey = getpass('private key:')
+encodedPrivateKey = base64.b64encode(privateKey.encode())
+print(privateKey)
+print(encodedPrivateKey)
 # nonassociated URI for auth purposes only
-redirectUri = "https://spotify-project/callback/"
+# redirectUri = "https://spotify-project/callback/"
 
-# url = 'https://api.spotify.com/v1/users/daveyavarez'
+grantType = "client_credentials"
+authUrl = 'https://accounts.spotify.com/api/token'
+params = [{'grant_type':grantType}]
+headers = (clientId, privateKey)
+bodyParams = {'grant_type': grantType}
+# paramsJson = json.dumps(params)
+# headersJson = json.dumps(headers)
 
-authUrl = 'https://accounts.spotify.com/authorize'
-# userName = input("username:")
-# password = getpass('password:')
-params = [{'client_id': clientId, 'response_type': 'code', 'redirect_uri': redirectUri}]
-import json
-paramsJson = json.dumps(params)
-r = requests.get(authUrl, data=paramsJson)
-print(r.status_code)
+
+r = requests.post(authUrl, data=bodyParams, auth=headers)
+print('status code is '+ str(r.status_code))
+print('body is '+ str(r.content))
